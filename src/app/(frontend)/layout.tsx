06746918e70dc028778ace04995e1dae920bf1ea
Awinel <benjamin.huerta.awi@gmail.com>
2025-08-22 13:49:49 -0400

    Implement theme

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '@/globals.css'
+import { ThemeProvider } from '@/components/theme/theme-provider'
 
 export const metadata = {
   description: 'A template to create your own portfolio',
@@ -10,9 +11,16 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <main>{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Portfolio from '@/lib/actions/portfolio'
+import { ThemeToggle } from '@/components/theme/theme-toggle'
 
 export default async function HomePage() {
   const portfolio = await Portfolio()
@@ -7,8 +8,12 @@ export default async function HomePage() {
     <>
       <header>
         <nav></nav>
-        <h1 className="text-red-500">{portfolio.header.name}</h1>
+        <h1 className="text-foreground">{portfolio.header.name}</h1>
       </header>
+      <main></main>
+      <footer>
+        <ThemeToggle />
+      </footer>
     </>
   )
 }
